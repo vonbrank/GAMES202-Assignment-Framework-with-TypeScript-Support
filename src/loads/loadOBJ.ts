d4ccb35b151e447a675894e9f2b9660011e42714
vonbrank <vonbrank@outlook.com>
2023-08-25 14:30:16 +0800

    fix (loadOBJ): 将类型断言修改为使用 instanceof 判断

--- src/loads/loadOBJ.ts
+++ src/loads/loadOBJ.ts
@@ -37,7 +37,7 @@ export function loadOBJ(
             }
         }
     }
-    function onError() {}
+    function onError() { }
 
     new MTLLoader(manager)
         .setPath(path)
@@ -50,16 +50,21 @@ export function loadOBJ(
                     name + ".obj",
                     function (object) {
                         object.traverse(function (event) {
-                            const child = event as THREE.Mesh;
-                            if (child.isMesh) {
+                            const child = event;
+
+                            if (child instanceof THREE.Mesh && child.isMesh) {
                                 let geo = child.geometry;
-                                let mat: THREE.MeshPhongMaterial;
-                                if (Array.isArray(child.material))
-                                    mat = child
-                                        .material[0] as THREE.MeshPhongMaterial;
-                                else
-                                    mat =
-                                        child.material as THREE.MeshPhongMaterial;
+                                let mat = new THREE.MeshPhongMaterial;
+                                if (Array.isArray(child.material)) {
+                                    let firstMaterial = child
+                                        .material[0];
+                                    if (firstMaterial instanceof THREE.MeshPhongMaterial) {
+                                        mat = firstMaterial;
+                                    }
+                                }
+                                else if (child.material instanceof THREE.MeshPhongMaterial) {
+                                    mat = child.material;
+                                }
 
                                 var indices = Array.from(
                                     { length: geo.attributes.position.count },
@@ -69,23 +74,16 @@ export function loadOBJ(
                                 let mesh = new Mesh(
                                     {
                                         name: "aVertexPosition",
-                                        array: new Float32Array(
-                                            (
-                                                geo.attributes
-                                                    .position as BufferAttribute
-                                            ).array
-                                        ),
+                                        array: new Float32Array(geo.attributes.position instanceof BufferAttribute ? geo.attributes.position.array : []),
                                     },
                                     {
                                         name: "aNormalPosition",
-                                        array: new Float32Array(
-                                            (
-                                                geo.attributes
-                                                    .normal as BufferAttribute
-                                            ).array
-                                        ),
+                                        array: new Float32Array(geo.attributes.normal instanceof BufferAttribute ? geo.attributes.normal.array : []),
                                     },
-                                    //{ name: 'aTextureCoord', array: geo.attributes.uv.array },
+                                    // {
+                                    //     name: "aTextureCoord",
+                                    //     array: new Float32Array(geo.attributes.uv instanceof BufferAttribute ? geo.attributes.uv.array : []),
+                                    // },
                                     null,
                                     indices,
                                     transform
