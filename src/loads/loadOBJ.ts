47fa94f0e3b37010bcd48279f64da0fc43f0b1f8
vonbrank <vonbrank@outlook.com>
2023-08-25 14:21:02 +0800

    fix (loadOBJ): 将类型断言修改为使用 instanceof 判断

--- src/loads/loadOBJ.ts
+++ src/loads/loadOBJ.ts
@@ -25,7 +25,7 @@ export function loadOBJ(renderer: WebGLRenderer, path: string, name: string) {
             );
         }
     }
-    function onError() {}
+    function onError() { }
 
     new MTLLoader(manager)
         .setPath(path)
@@ -38,17 +38,21 @@ export function loadOBJ(renderer: WebGLRenderer, path: string, name: string) {
                     name + ".obj",
                     function (object) {
                         object.traverse(function (event) {
-                            const child = event as THREE.Mesh;
+                            const child = event;
 
-                            if (child.isMesh) {
+                            if (child instanceof THREE.Mesh && child.isMesh) {
                                 let geo = child.geometry;
-                                let mat: THREE.MeshPhongMaterial;
-                                if (Array.isArray(child.material))
-                                    mat = child
-                                        .material[0] as THREE.MeshPhongMaterial;
-                                else
-                                    mat =
-                                        child.material as THREE.MeshPhongMaterial;
+                                let mat = new THREE.MeshPhongMaterial;
+                                if (Array.isArray(child.material)) {
+                                    let firstMaterial = child
+                                        .material[0];
+                                    if (firstMaterial instanceof THREE.MeshPhongMaterial) {
+                                        mat = firstMaterial;
+                                    }
+                                }
+                                else if (child.material instanceof THREE.MeshPhongMaterial) {
+                                    mat = child.material;
+                                }
 
                                 var indices = Array.from(
                                     { length: geo.attributes.position.count },
@@ -57,30 +61,15 @@ export function loadOBJ(renderer: WebGLRenderer, path: string, name: string) {
                                 let mesh = new Mesh(
                                     {
                                         name: "aVertexPosition",
-                                        array: new Float32Array(
-                                            (
-                                                geo.attributes
-                                                    .position as BufferAttribute
-                                            ).array
-                                        ),
+                                        array: new Float32Array(geo.attributes.position instanceof BufferAttribute ? geo.attributes.position.array : []),
                                     },
                                     {
                                         name: "aNormalPosition",
-                                        array: new Float32Array(
-                                            (
-                                                geo.attributes
-                                                    .normal as BufferAttribute
-                                            ).array
-                                        ),
+                                        array: new Float32Array(geo.attributes.normal instanceof BufferAttribute ? geo.attributes.normal.array : []),
                                     },
                                     {
                                         name: "aTextureCoord",
-                                        array: new Float32Array(
-                                            (
-                                                geo.attributes
-                                                    .uv as BufferAttribute
-                                            ).array
-                                        ),
+                                        array: new Float32Array(geo.attributes.uv instanceof BufferAttribute ? geo.attributes.uv.array : []),
                                     },
                                     indices
                                 );
@@ -109,7 +98,7 @@ export function loadOBJ(renderer: WebGLRenderer, path: string, name: string) {
                                             },
                                             uKd: {
                                                 type: "3fv",
-                                                value: new Float32Array(mat.color.toArray()),
+                                                value: new Float32Array(mat.color.toArray() || []),
                                             },
                                         },
                                         [],
@@ -125,7 +114,7 @@ export function loadOBJ(renderer: WebGLRenderer, path: string, name: string) {
                                             },
                                             uKd: {
                                                 type: "3fv",
-                                                value: new Float32Array(mat.color.toArray()),
+                                                value: new Float32Array(mat.color.toArray() || []),
                                             },
                                         },
                                         [],
